eec662d8aadee91388c8d964666e7cc6051094d2
Sarath <sarath.hacks@gmail.com>
2016-02-19 20:41:19 +0530

    Added property

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>Hello {{name}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+
+	constructor(){
+		this.name = "World"
+	}
+ }
